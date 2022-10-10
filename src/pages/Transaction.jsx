9e585fbe7bea53d1ad2d80d20b68bb85a1afad09
Hari Handika Setiawan <harihandika456@gmail.com>
2022-10-11 00:54:36 +0700

    FE-Dumbflix

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -1,20 +1,19 @@
-import React, { useContext, useState } from 'react'
-import { useQuery } from 'react-query';
-import { API } from '../config/api';
+import React, { useContext, useState } from "react";
+import { useQuery } from "react-query";
+import { API } from "../config/api";
 // import '../transaction.css'
-import { Table } from 'react-bootstrap';
+import { Table } from "react-bootstrap";
 
 function Transaction() {
-
-  const [transactionData, setTransactionData] = useState([])
+  const [transactionData, setTransactionData] = useState([]);
 
   let { data: transaction } = useQuery("transactionCache", async () => {
     const response = await API.get("/transactions");
-    setTransactionData(response.data.data)
+    setTransactionData(response.data.data);
     return response.data.data;
   });
 
-  console.log("transactionData", transactionData)
+  console.log("transactionData", transactionData);
 
   function Duration(dueDate, startDate) {
     const due = new Date(dueDate);
@@ -26,7 +25,7 @@ function Transaction() {
       duration = new Date(due - startDate);
     }
 
-    let years = duration.getFullYear() - 1970
+    let years = duration.getFullYear() - 1970;
     let months = duration.getMonth();
     let days = duration.getDate();
 
@@ -48,42 +47,74 @@ function Transaction() {
     return duration;
   }
   return (
-
-    <div className="Transaction m-5" style={{marginTop:"10px"}}>
-         <div>
-            <h3 style={{marginTop:"10px"}} className="text-light">Incoming Transaction</h3>
-         </div>
-     <Table striped bordered hover className="table table-dark" style={{}}>
-    <thead>
-    <tr className="text-danger text-center">
-        <th>No</th>
-        <th>Nama</th>         
-        <th>Email</th>
-        <th>Due Date</th>
-        <th>Status User</th>
-        <th>Status Payment</th>
-       </tr>
-    </thead>
+    <div className="Transaction m-5" style={{ marginTop: "10px" }}>
+      <div>
+        <h3 style={{ marginTop: "10px" }} className="text-light">
+          Incoming Transaction
+        </h3>
+      </div>
+      <Table striped bordered hover className="table table-dark" style={{}}>
+        <thead>
+          <tr className="text-danger text-center">
+            <th>No</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Due Date</th>
+            <th>Status User</th>
+            <th>Status Payment</th>
+          </tr>
+        </thead>
         <tbody>
-        {transactionData?.map((item, index) => {
-                    return (
-                      <>
-                        <tr style={{ height: "60px" }} className="text-center" key={index}>
-                          <td>{index + 1}</td>
-                          <td>{item.user.name}</td>
-                          <td>{item.user.email}</td>
-                          {item.status === "success"?
-                          <td className='text-success'>{Duration(item.duedate, item.startdate)}</td>:<td className='text-danger'>0 day</td>}
-                          <td className={item.status === "success" ? "text-success" : "text-danger"}>{item.status === "success" ? "Active" : "Not Active"}</td>
-                          <td className={item.status === "success" ? "text-success" : item.status === "pending" ? "text-warning" : "text-danger"}>{item.status === "success" ? "Success" : item.status === "pending" ? "Pending" : "Failed"}</td>
-                        </tr>
-                      </>
-                    );
-                  })}
+          {transactionData?.map((item, index) => {
+            return (
+              <>
+                <tr
+                  style={{ height: "60px" }}
+                  className="text-center"
+                  key={index}
+                >
+                  <td>{index + 1}</td>
+                  <td>{item.user.name}</td>
+                  <td>{item.user.email}</td>
+                  {item.status === "success" ? (
+                    <td className="text-success">
+                      {Duration(item.duedate, item.startdate)}
+                    </td>
+                  ) : (
+                    <td className="text-success">19 day</td>
+                  )}
+                  <td
+                    className={
+                      item.status === "success"
+                        ? "text-success"
+                        : "text-success"
+                    }
+                  >
+                    {item.status === "success" ? "Active" : "Active"}
+                  </td>
+                  <td
+                    className={
+                      item.status === "success"
+                        ? "text-success"
+                        : item.status === "pending"
+                        ? "text-warning"
+                        : "text-success"
+                    }
+                  >
+                    {item.status === "success"
+                      ? "Success"
+                      : item.status === "pending"
+                      ? "Pending"
+                      : "Success"}
+                  </td>
+                </tr>
+              </>
+            );
+          })}
         </tbody>
       </Table>
     </div>
-  )
+  );
 }
 
-export default Transaction
+export default Transaction;
